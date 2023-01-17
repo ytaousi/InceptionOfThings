41a26738cdef715bd21a3193804567ecee4a07bd
ytaousi <yassir.taous@gmail.com>
2023-01-17 21:07:26 +0100

    need to fix ssh from host machine , and reachability from worker to the node

--- p1/initScripts/masterScript.sh
+++ p1/initScripts/masterScript.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-yum update -y
-yum install -y telnet
+apt-get update
+apt-get upgrade -y
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=" --write-kubeconfig-mode 644 server --flannel-iface=eth1" sh -
-# cp /var/lib/rancher/k3s/server/node-token /some/synced/path
-# cp /etc/rancher/k3s/k3s.yaml /some/synced/path
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrantShared
+sudo cp /etc/rancher/k3s/k3s.yaml /vagrantShared
