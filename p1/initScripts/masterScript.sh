1daac0b1fec040aaa50c23d788f868901d40189c
ytaousi <yasssir.taous@gmail.com>
2023-09-07 17:49:12 +0100

    part1 finished

--- p1/initScripts/masterScript.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-apt-get update
-apt-get upgrade -y
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=" --write-kubeconfig-mode 644 server --flannel-iface=eth1" sh -
-sudo cp /var/lib/rancher/k3s/server/node-token /vagrantShared
-sudo cp /etc/rancher/k3s/k3s.yaml /vagrantShared
--- p1/initScripts/workerScript.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-apt-get update
-apt-get upgrade -y
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface=eth1" K3S_TOKEN=$(cat /vagrantShared/node-token) K3S_URL=https://192.168.56.110:6443 sh -
